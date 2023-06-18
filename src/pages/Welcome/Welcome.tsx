97bc65a7e035290ffe979e19cfa044c187860ac3
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-06-18 15:47:30 +0400

    Style the welcome page

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,6 +1,6 @@
 import cl from './Welcome.module.sass';
 // import { Link } from 'react-router-dom';
-import { Button } from '@mui/material';
+import { Button, Container } from '@mui/material';
 // import { Theme } from '@emotion/react';
 // import { createTheme } from '@mui/material/styles';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
@@ -9,14 +9,14 @@ import Header from '../../layouts/Header/Header';
 export default function Welcome() {
   return (
     // container only for background
-    <div className={cl.container}>
-      <div className={cl.content}>
-        <Header>{/* <img src='../../assets/' alt='' /> */}</Header>
-        <div className={cl.body}>
+    <div className={cl.background}>
+      <div className={cl.container}>
+        <Header />
+        <div className={cl.content}>
           <h1 className={cl.title}>
             Welcome <br /> to the Kessoku Band Store
           </h1>
-          <h2 className={cl.subtitle}>
+          <h2 className={cl.slogan}>
             we became brave enough to open the store
           </h2>
           <Button
