fd0d00bb6b41228f91acfd01ebdc400db6fac59b
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-06-21 23:53:29 +0400

    Rename main.tsx to index.tsx and add a browser router

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './styles/theme.ts';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
