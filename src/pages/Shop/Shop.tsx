5399799d9a8c2f2820e7b700c99e214321c1913e
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-06-22 15:49:14 +0400

    Add the shop component
    Will be used as the main page with products. For now has only header and footer

---
+++ src/pages/Shop/Shop.tsx
@@ -0,0 +1,27 @@
+import Header from '../../layouts/Header/Header';
+import Main from '../../layouts/Main/Main';
+import Footer from '../../layouts/Footer/Footer';
+import { Container } from '@mui/material';
+
+export default function Shop() {
+  return (
+    <Container
+      sx={{
+        width: '100vw',
+        minHeight: '100vh',
+        backgroundColor: 'whitesmoke',
+        padding: '0 40px',
+        display: 'flex',
+        color: '#6f6e6e',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+      maxWidth={false}
+    >
+      <Header isDark={false} />
+      <Main />
+      <Footer isDark={false} />
+    </Container>
+  );
+}
