03609a3eb3e66ef699f8b62c60b498d8a6b03215
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-07-01 19:13:57 +0400

    Refactor header component
    Use MUI Box component instead of HTML header tag. Use inline styling instead of SASS

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,37 +1,44 @@
-import cl from './Header.module.sass';
 import { Input, Box } from '@mui/material';
 import CartButton from '../../components/UI/CartButton/CartButton';
 import Logo from '../../components/UI/Logo/Logo';
 
 export default function Header({ isDark }: { isDark: boolean }) {
-  const inputStyling = isDark
-    ? { style: { color: 'whitesmoke', width: '240px' } }
-    : { style: { color: '#6f6e6e', width: '240px' } };
+	const inputStyling = isDark
+		? { style: { color: 'whitesmoke', width: '240px' } }
+		: { style: { color: '#6f6e6e', width: '240px' } };
 
-  return (
-    <header className={cl.header}>
-      <Box sx={{ width: '100%' }}>
-        <Logo props={{ isDark: isDark, size: '56px' }} />
-      </Box>
-      <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-        <form>
-          <Input
-            placeholder='Search...'
-            color='secondary'
-            inputProps={{ ...inputStyling }}
-          />
-        </form>
-        <CartButton
-          props={{
-            size: 'small',
-            color: 'primary',
-            badgeValue: 4,
-            badgeMaxValue: 9,
-            badgeColor: 'secondary',
-            iconSize: 'large',
-          }}
-        />
-      </Box>
-    </header>
-  );
+	return (
+		<Box
+			component={'header'}
+			sx={{
+				display: 'flex',
+				width: '100%',
+				justifyContent: 'space-around',
+				padding: '16px 0',
+			}}
+		>
+			<Box sx={{ width: '100%' }}>
+				<Logo props={{ isDark: isDark, size: '56px' }} />
+			</Box>
+			<Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+				<form>
+					<Input
+						placeholder='Search...'
+						color='secondary'
+						inputProps={{ ...inputStyling }}
+					/>
+				</form>
+				<CartButton
+					props={{
+						size: 'small',
+						color: 'primary',
+						badgeValue: 4,
+						badgeMaxValue: 9,
+						badgeColor: 'secondary',
+						iconSize: 'large',
+					}}
+				/>
+			</Box>
+		</Box>
+	);
 }
