f38ed467fc6477a2367bb3f63a220c06728003a0
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-07-01 16:53:06 +0400

    Remove padding

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -4,24 +4,23 @@ import Footer from '../../layouts/Footer/Footer';
 import { Container } from '@mui/material';
 
 export default function Shop() {
-  return (
-    <Container
-      sx={{
-        width: '100vw',
-        minHeight: '100vh',
-        backgroundColor: 'whitesmoke',
-        padding: '0 40px',
-        color: '#6f6e6e',
-        display: 'flex',
-        flexDirection: 'column',
+	return (
+		<Container
+			sx={{
+				width: '100vw',
+				minHeight: '100vh',
+				backgroundColor: 'whitesmoke',
+				color: '#6f6e6e',
+				display: 'flex',
+				flexDirection: 'column',
 
-        alignItems: 'center',
-      }}
-      maxWidth={false}
-    >
-      <Header isDark={false} />
-      <Main />
-      <Footer isDark={false} />
-    </Container>
-  );
+				alignItems: 'center',
+			}}
+			maxWidth={false}
+		>
+			<Header isDark={false} />
+			<Main />
+			<Footer isDark={false} />
+		</Container>
+	);
 }
