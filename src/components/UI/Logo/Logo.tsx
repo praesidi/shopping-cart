e1be146115f09c5ffce77feb4c117c664c665320
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-07-03 21:08:07 +0400

    Update urls

--- src/components/UI/Logo/Logo.tsx
+++ src/components/UI/Logo/Logo.tsx
@@ -1,20 +1,20 @@
 import { Link } from 'react-router-dom';
-import Image from '../../../assets/logo.svg';
+import Image from '../../../assets/images/logo.svg';
 
 export default function Logo({ props }: any) {
-  const { isDark, size } = props;
-  return (
-    <Link to={'/'}>
-      {isDark ? (
-        <img src={Image} height={size} alt='logo' />
-      ) : (
-        <img
-          src={Image}
-          height={size}
-          style={{ filter: 'brightness(0.45)' }}
-          alt='logo'
-        />
-      )}
-    </Link>
-  );
+	const { isDark, size } = props;
+	return (
+		<Link to={'/'}>
+			{isDark ? (
+				<img src={Image} height={size} alt='logo' />
+			) : (
+				<img
+					src={Image}
+					height={size}
+					style={{ filter: 'brightness(0.45)' }}
+					alt='logo'
+				/>
+			)}
+		</Link>
+	);
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,14 @@ import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material';
-import { theme } from './styles/theme.ts';
+import { theme } from './assets/styles/theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
+		</BrowserRouter>
+	</React.StrictMode>
 );
