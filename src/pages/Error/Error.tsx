28dce99bff935a2db98102e248208caa36d45660
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-07-31 18:30:02 +0300

    Refactor Error page

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -11,34 +11,28 @@ export default function Error() {
 				height: '100vh',
 				backgroundColor: 'whitesmoke',
 				color: '#6f6e6e',
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'center',
+				alignItems: 'center',
+				gap: '20px',
 			}}
 			maxWidth={false}
 		>
-			<Box
-				sx={{
-					display: 'flex',
-					flexDirection: 'column',
-					justifyContent: 'center',
-					alignItems: 'center',
-					gap: '20px',
-					height: '100%',
-				}}
-			>
-				<img src={image} alt='error illustration' height={400} />
-				<Typography variant='h2'>Ooops... Something went wrong :(</Typography>
-				<Typography variant='body2'>
-					<Link to='/products'>
-						<Button
-							variant='outlined'
-							fullWidth={false}
-							color='secondary'
-							startIcon={<ArrowBackIcon />}
-						>
-							return to the shop page
-						</Button>
-					</Link>
-				</Typography>
-			</Box>
+			<Box component='img' src={image} height={250} alt='error illustration' />
+			<Typography variant='h2'>Ooops... Something went wrong :(</Typography>
+			<Typography variant='body2'>
+				<Link to='/products'>
+					<Button
+						variant='outlined'
+						fullWidth={false}
+						color='secondary'
+						startIcon={<ArrowBackIcon />}
+					>
+						return to the shop page
+					</Button>
+				</Link>
+			</Typography>
 		</Container>
 	);
 }
