837044ff15d74f2f290491f8ec61bf15b63f75df
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-08-01 13:45:56 +0300

    Add product gallery

---
+++ src/components/ProductGallery/ProductGallery.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+import styles from './ProductGallery.module.sass';
+
+export default function ProductGallery(props: any) {
+	const [thumbsSwiper, setThumbsSwiper] = useState<any>(null);
+
+	// used inline styling for height bcs swiper resets the values for some reason
+	return (
+		<>
+			<Swiper
+				style={
+					{
+						'--swiper-navigation-color': 'lightgray',
+						'--swiper-pagination-color': 'lightgray',
+						height: '80%',
+					} as React.CSSProperties
+				}
+				loop={true}
+				spaceBetween={10}
+				navigation={true}
+				thumbs={{
+					swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
+				}}
+				modules={[FreeMode, Navigation, Thumbs]}
+				className={styles.mySwiper}
+			>
+				{props.images?.map((item: string, index: number) => (
+					<SwiperSlide key={index}>
+						<img src={item} alt='product images' />
+					</SwiperSlide>
+				))}
+			</Swiper>
+			<Swiper
+				onSwiper={setThumbsSwiper}
+				spaceBetween={10}
+				slidesPerView={4}
+				freeMode={true}
+				watchSlidesProgress={true}
+				modules={[FreeMode, Navigation, Thumbs]}
+				className={styles.mySwiper2}
+				style={{ height: '20%' }}
+			>
+				{props.images?.map((item: string, index: number) => (
+					<SwiperSlide key={index}>
+						<img src={item} alt='product images' />
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</>
+	);
+}
