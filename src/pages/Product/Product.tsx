faea651ab9b93ca9815c00a9854be4176f946070
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-08-01 13:48:55 +0300

    Add grid layout and product gallery to the page

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,9 +1,21 @@
 import Header from '../../layouts/Header/Header';
 import Footer from '../../layouts/Footer/Footer';
-import ProductBody from '../../layouts/ProductBody/ProductBody';
 import { Container, Box } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
+import ProductGallery from '../../components/ProductGallery/ProductGallery';
+import image1 from '../../assets/images/empty-cart.png';
+import image2 from '../../assets/images/error.png';
 
 export default function Product() {
+	const bocchies = [
+		image1,
+		image2,
+		'https://swiperjs.com/demos/images/nature-1',
+		'https://swiperjs.com/demos/images/nature-2',
+		'https://swiperjs.com/demos/images/nature-3',
+		'https://swiperjs.com/demos/images/nature-4',
+	];
+
 	return (
 		<Container
 			sx={{
@@ -19,7 +31,30 @@ export default function Product() {
 			maxWidth={false}
 		>
 			<Header isDark={false} />
-			<ProductBody />
+			<Container
+				sx={{
+					display: 'flex',
+					justifyContent: 'space-evenly',
+					padding: '0 24px',
+					margin: '40px 0 80px',
+				}}
+				disableGutters
+				maxWidth={false}
+			>
+				<Grid container columns={{ xs: 6, md: 8 }} spacing={2}>
+					<Grid xs={6} md={4}>
+						<Box
+							sx={{ height: '500px', backgroundColor: '#fff', padding: '8px' }}
+						>
+							<ProductGallery images={bocchies} />
+						</Box>
+					</Grid>
+					<Grid xs={6} md={4}>
+						<Box>Title</Box>
+						<Box>Description</Box>
+					</Grid>
+				</Grid>
+			</Container>
 			<Footer isDark={false} />
 		</Container>
 	);
