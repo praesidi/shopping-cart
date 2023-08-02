6e82593c88511287c7c9e53a9a957306665e2a0f
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-08-02 19:56:24 +0300

    Add content to the page

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,23 +1,35 @@
 import Header from '../../layouts/Header/Header';
 import Footer from '../../layouts/Footer/Footer';
-import { Container, Box, Typography } from '@mui/material';
+import { Container, Box, Typography, Button } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import ProductGallery from '../../components/ProductGallery/ProductGallery';
 import imagePlaceholder from '../../assets/images/image-placeholder.jpeg';
+import { Link } from 'react-router-dom';
 
-export default function Product(props: any) {
-	const imagesDefault = [imagePlaceholder, imagePlaceholder];
+export default function Product({
+	title = 'Product Name',
+	category = 'Product Category',
+	description = 'Product description where we are trying to convince a customer to buy this useless piece of crap',
+	price = 69420,
+	image = [imagePlaceholder],
+}) {
+	const imagesDefault = [imagePlaceholder];
 
+	// TODO: check how responsive pages are
+	// FIXME: fix bug when image slider collapses if there is only 1 image (width fixes it in some way)
+	// bug above fixed by itself (still needs testing)
 	return (
 		<Container
 			sx={{
 				width: '100vw',
+				maxWidth: '1400px',
 				minHeight: '100vh',
 				backgroundColor: 'whitesmoke',
 				color: '#6f6e6e',
 				display: 'flex',
 				flexDirection: 'column',
-
 				alignItems: 'center',
 			}}
 			maxWidth={false}
@@ -26,32 +38,113 @@ export default function Product(props: any) {
 			<Container
 				sx={{
 					display: 'flex',
+					flexDirection: 'column',
 					justifyContent: 'space-evenly',
 					padding: '0 24px',
-					margin: '40px 0 80px',
+					marginBottom: '64px',
+					// minHeight: '100%',
 				}}
 				disableGutters
 				maxWidth={false}
 			>
-				<Grid container columns={{ xs: 6, md: 8 }} spacing={2}>
+				<Box sx={{ marginBottom: '12px' }}>
+					<Link to={'/products'}>
+						<Button startIcon={<ArrowBackIcon />}>back to store</Button>
+					</Link>
+				</Box>
+				<Grid container columns={{ xs: 6, md: 8 }} spacing={3}>
 					<Grid xs={6} md={4}>
 						<Box
-							sx={{ height: '500px', backgroundColor: '#fff', padding: '8px' }}
+							sx={{
+								height: '500px',
+								backgroundColor: '#fff',
+								padding: '8px',
+							}}
 						>
-							<ProductGallery
-								images={props.image ? props.image : imagesDefault}
-							/>
+							<ProductGallery images={image ? image : imagesDefault} />
 						</Box>
 					</Grid>
 					<Grid xs={6} md={4}>
-						<Box>
-							<Typography variant='h1'>{props.title}</Typography>
-							<Typography variant='subtitle1'>{props.category}</Typography>
-						</Box>
-						<Box>
-							<Typography variant='h2'>
-								<Typography variant='body1'>{props.description}</Typography>
-							</Typography>
+						<Box
+							sx={{
+								display: 'flex',
+								flexDirection: 'column',
+								gap: '30px',
+								height: '100%',
+							}}
+						>
+							<Box>
+								<Typography
+									variant='h1'
+									sx={{ fontSize: '3rem', fontWeight: 300 }}
+								>
+									{title}
+								</Typography>
+								<Typography
+									variant='h2'
+									sx={{ fontSize: '1.5rem', fontWeight: 300 }}
+								>
+									{category}
+								</Typography>
+							</Box>
+							<Box>
+								<Typography
+									variant='h3'
+									sx={{ fontSize: '1.8rem', fontWeight: 400 }}
+								>
+									Description
+								</Typography>
+								<Typography variant='body1' sx={{ fontSize: '1.2rem' }}>
+									{description}
+								</Typography>
+							</Box>
+							<Box
+								sx={{
+									display: 'flex',
+									flexDirection: 'column',
+									justifyContent: 'center',
+									alignItems: 'center',
+									marginTop: 'auto',
+								}}
+							>
+								<Box
+									sx={{
+										width: '100%',
+										display: 'flex',
+										justifyContent: 'flex-start',
+										marginBottom: '16px',
+									}}
+								>
+									<Typography
+										variant='h3'
+										sx={{
+											fontSize: '1.8rem',
+											fontWeight: 500,
+										}}
+									>
+										Price:
+										<Box
+											component={'span'}
+											sx={{
+												fontWeight: 300,
+												fontStyle: 'italic',
+											}}
+										>
+											{' '}
+											${price}
+										</Box>
+									</Typography>
+								</Box>
+								<Button
+									sx={{ width: '80%' }}
+									variant='outlined'
+									color='secondary'
+									fullWidth
+									endIcon={<ShoppingCartIcon />}
+								>
+									Add to cart
+								</Button>
+							</Box>
 						</Box>
 					</Grid>
 				</Grid>
