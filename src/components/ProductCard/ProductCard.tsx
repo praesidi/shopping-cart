e026a804eb327257032425a45519ab47d78a3352
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-08-04 16:44:06 +0300

    Make card keep the same size regardless of content

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,41 +1,59 @@
 import { styled } from '@mui/material/styles';
 import {
-	Paper,
 	Card,
 	Typography,
 	CardActionArea,
 	CardMedia,
 	CardContent,
 	CardActions,
-	CardHeader,
 	Button,
 } from '@mui/material/';
+import { Link } from 'react-router-dom';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
-const url =
-	'https://images-na.ssl-images-amazon.com/images/I/71qmF0FHj7L._AC_SX679_.jpg';
+interface Product {
+	id: number;
+	title: string;
+	category: string;
+	price: number;
+	description: string;
+}
 
-export default function ProductCard({ product }: { product: any }) {
+export default function ProductCard({ product }: { product: Product }) {
 	return (
 		<Card
-			sx={{ minWidth: '280px', minHeight: '320px', margin: 0, padding: '4px' }}
+			sx={{
+				minWidth: '280px',
+				width: '90%',
+				minHeight: '320px',
+				height: '100%',
+				margin: 0,
+				padding: '4px',
+				display: 'flex',
+				flexDirection: 'column',
+			}}
 		>
-			<CardActionArea>
+			<CardActionArea
+				sx={{ height: '90%' }}
+				component={Link}
+				to={`./${product.id}`}
+			>
 				<CardMedia
 					component='img'
-					height='240'
-					image={url}
-					sx={{ aspectRatio: '4 / 3' }}
+					image={product.image}
+					sx={{
+						aspectRatio: '4 / 3',
+						objectFit: 'contain',
+						padding: '5px',
+						height: '50%',
+					}}
 					alt='green iguana'
 				/>
-				<CardContent>
-					<Typography
-						gutterBottom
-						variant='h5'
-						component='div'
-						sx={{ margin: 0 }}
-					>
-						{product.name}
+				<CardContent
+					sx={{ display: 'flex', flexDirection: 'column', height: '50%' }}
+				>
+					<Typography gutterBottom variant='h5' sx={{ margin: 0 }}>
+						{product.title}
 					</Typography>
 					<Typography
 						variant='body2'
@@ -47,14 +65,16 @@ export default function ProductCard({ product }: { product: any }) {
 					<Typography
 						variant='body2'
 						color='text.secondary'
-						sx={{ fontSize: '24px' }}
+						sx={{ fontSize: '24px', marginTop: 'auto' }}
 					>
 						{'$'}
 						{product.price}
 					</Typography>
 				</CardContent>
 			</CardActionArea>
-			<CardActions sx={{ display: 'flex', justifyContent: 'center' }}>
+			<CardActions
+				sx={{ display: 'flex', justifyContent: 'center', marginTop: 'auto' }}
+			>
 				<Button
 					variant='outlined'
 					color='secondary'
