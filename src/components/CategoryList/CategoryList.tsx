fe7b183fe9c993662fdbb1ea8e5091f89d353d4f
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-08-04 19:00:35 +0300

    Add 'all' category to categories fetched from API
    Will allow to user switch between all products and products from certain category

--- src/components/CategoryList/CategoryList.tsx
+++ src/components/CategoryList/CategoryList.tsx
@@ -1,7 +1,11 @@
 import { Box, List, ListItemButton, ListItemText } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-export default function CategoryList({ categories }: { categories: string[] }) {
+export default function CategoryList({
+	categories,
+}: {
+	categories: string[] | null;
+}) {
 	const [selectedIndex, setSelectedIndex] = useState(0);
 
 	const handleClick = (
@@ -10,14 +14,22 @@ export default function CategoryList({ categories }: { categories: string[] }) {
 	) => {
 		setSelectedIndex(index);
 	};
+
+	if (!categories?.includes('all')) {
+		categories?.unshift('all');
+	}
+
 	return (
 		<Box>
 			<List component='div' aria-label='product categories'>
-				{categories.map((item, index) => {
+				{categories?.map((item, index) => {
 					return (
 						<ListItemButton
 							selected={selectedIndex === index}
 							onClick={(event) => handleClick(event, index)}
+							sx={{
+								textTransform: 'capitalize',
+							}}
 							key={index}
 						>
 							<ListItemText primary={item} />
