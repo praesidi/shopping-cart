c2109c925b559721be1d0415f1bcc20fb537860f
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-08-05 13:25:58 +0300

    Move shop page body from external component
    Allows reduce number of files, imports and improve DX

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -1,9 +1,46 @@
-import Header from '../../layouts/Header/Header';
-import Main from '../../layouts/Main/Main';
-import Footer from '../../layouts/Footer/Footer';
-import { Container } from '@mui/material';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
+import { useState } from 'react';
+import { Container, Box, CircularProgress } from '@mui/material';
+// import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Unstable_Grid2';
+import SortSelect from '../../components/UI/SortSelect/SortSelect';
+import ProductCard from '../../components/ProductCard/ProductCard';
+import CategoryList from '../../components/CategoryList/CategoryList';
+import useFetch from '../../hooks/useFetch';
+
+// TODO: set sidebar width 100%, flex wrap for mobile devices
+
+interface Product {
+	id: number;
+	title: string;
+	category: string;
+	price: number;
+	description: string;
+	image: string;
+}
 
 export default function Shop() {
+	const [sortBy, setSortBy] = useState('');
+	const [currentCategory, setCurrentCategory] = useState('all');
+
+	const { data: categories } = useFetch(
+		'https://fakestoreapi.com/products/categories'
+	);
+
+	const {
+		data: products,
+		isLoading,
+		error,
+	} = useFetch('https://fakestoreapi.com/products');
+
+	console.log(isLoading);
+
+	if (isLoading) return <CircularProgress color='secondary' size={80} />;
+
+	if (error) return <ErrorMessage />;
+
 	return (
 		<Container
 			sx={{
@@ -18,7 +55,67 @@ export default function Shop() {
 			maxWidth={false}
 		>
 			<Header isDark={false} />
-			<Main />
+			<Container
+				sx={{
+					display: 'flex',
+					justifyContent: 'space-evenly',
+					padding: '0 24px',
+					margin: '40px 0 80px',
+				}}
+				disableGutters
+				maxWidth={false}
+			>
+				{/* Sidebar */}
+				<Box
+					sx={{
+						marginRight: '24px',
+						display: 'flex',
+						flexDirection: 'column',
+						gap: '30px',
+						minWidth: '20%',
+					}}
+				>
+					<Box>
+						Shop /{' '}
+						<Box
+							component={'span'}
+							sx={{
+								fontWeight: 600,
+								fontSize: '1.5em',
+								textTransform: 'capitalize',
+							}}
+						>
+							{currentCategory}
+						</Box>
+					</Box>
+					<CategoryList categories={categories} />
+				</Box>
+				{/* Main Container with Products */}
+				<Box sx={{ display: 'flex', flexDirection: 'column', gap: '30px' }}>
+					<SortSelect sortBy={sortBy} setSortBy={setSortBy} />
+					<Grid
+						container
+						columns={{ xs: 2, md: 12, lg: 12 }}
+						spacing={{ xs: 2, md: 3, lg: 4 }}
+					>
+						{(products as Product[] | null)?.map((product) => {
+							return (
+								<Grid
+									xs={2}
+									md={6}
+									lg={4}
+									display='flex'
+									justifyContent='center'
+									alignItems='center'
+									key={product?.id}
+								>
+									<ProductCard product={product} />
+								</Grid>
+							);
+						})}
+					</Grid>
+				</Box>
+			</Container>
 			<Footer isDark={false} />
 		</Container>
 	);
