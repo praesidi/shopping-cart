45f733473cf85d51e179bb2d8d55ff60a3edfd8c
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-08-05 13:18:14 +0300

    Set isLoading value true by default

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -3,11 +3,10 @@ import axios from 'axios';
 
 export default function useFetch(url: string) {
 	const [data, setData] = useState(null);
-	const [isLoading, setIsLoading] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
 	const [error, setError] = useState(null);
 
 	useEffect(() => {
-		setIsLoading(true);
 		axios
 			.get(url)
 			.then((response) => {
