28d6b0755728bab05bce5d54f7f1b516cd6ccaa7
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-08-07 11:33:30 +0300

    Add Cart component to App component
    Allows to open cart from any page

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,16 @@ import Error from './pages/Error/Error';
 import Product from './pages/Product/Product';
 import Cart from './components/Cart/Cart';
 
-// FIXME: fix error page error (something related to props)
-
 export default function App() {
 	return (
-		<Routes>
-			<Route path='/' element={<Welcome />} />
-			<Route path='/products' element={<Shop />} />
-			<Route path='/products/:id' element={<Product />} />
-			<Route path='*' element={<Error />} />
-		</Routes>
+		<>
+			<Routes>
+				<Route path='/' element={<Welcome />} />
+				<Route path='/products' element={<Shop />} />
+				<Route path='/products/:id' element={<Product />} />
+				<Route path='*' element={<Error />} />
+			</Routes>
+			<Cart />
+		</>
 	);
 }
