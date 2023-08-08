d9cd533ff7491e2d14c0811cd40d670217bd3d52
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-08-08 13:47:33 +0300

    Add storing cart products in local storage

--- src/store/shoppingCart/cartProductsSlice.ts
+++ src/store/shoppingCart/cartProductsSlice.ts
@@ -1,12 +1,20 @@
-import { createSlice, Middleware, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Product } from '../../types';
 
+const itemsFromLocalStorage = localStorage.getItem('cart');
+
+const products =
+	itemsFromLocalStorage !== null ? JSON.parse(itemsFromLocalStorage) : [];
+
+const setItem = (item: Product[]) => {
+	localStorage.setItem('cart', JSON.stringify(item));
+};
 interface CartState {
 	productsInCart: Product[];
 }
 
 const initialState: CartState = {
-	productsInCart: [],
+	productsInCart: products,
 };
 
 export const cartProductsSlice = createSlice({
@@ -15,15 +23,16 @@ export const cartProductsSlice = createSlice({
 	reducers: {
 		addToCart: (state, action: PayloadAction<Product>) => {
 			state.productsInCart.push(action.payload);
+			setItem(state.productsInCart);
 		},
 		removeFromCart: (state, action: PayloadAction<Product>) => {
-			console.log(action.payload);
 			for (let i = 0; i < state.productsInCart.length; i++) {
 				if (state.productsInCart[i].id === action.payload.id) {
 					state.productsInCart.splice(i, 1);
 					break;
 				}
 			}
+			setItem(state.productsInCart);
 		},
 	},
 });
